57110d5e0b11e9bfa21aafe0b6896448e71cbc87
Priyangshu <shomepriyangshu16@gmail.com>
2025-04-25 02:25:30 +0530

    updated package.json and downgraded to react v.18.2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import "@excalidraw/excalidraw/index.css";
 
 
 createRoot(document.getElementById("root")).render(
